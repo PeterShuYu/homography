a31218964c842e70df8aa13e32a62de10079391c
PeterShuYu <98564473+PeterShuYu@users.noreply.github.com>
2022-01-31 01:50:33 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -77,7 +77,74 @@ def photo():
     histr = cv2.calcHist([image],[0],None,[256],[0,256])
     st.bar_chart(histr)
     
-    '''
+    # my own start
+
+    # Setting Parameter
+    phi = 25 # [-70~70]
+             # unit degree
+    scale_factor = 1 #(this is optional)
+
+    k = 0.7 # need to be positive-value
+    b = 0.5 # fix
+
+    #increment = np.tan((phi/180)*3.14)
+    #l_side = np.sqrt(5) + increment
+    #r_side = np.sqrt(5) - increment
+
+    increment = ((k+b)/b)*np.tan((phi/180)*3.14)
+    l_side = np.sqrt( ((k+b)/b)**2 + (k+b)**2 ) + increment
+    r_side = np.sqrt( ((k+b)/b)**2 + (k+b)**2 ) - increment
+
+
+    origin_len = img.shape[0]/2
+    origin_wid = img.shape[1]/2
+
+    transform_center = [ origin_wid , origin_len ]
+    #transform_l = (0.5*np.sqrt(5)/l_side)*origin_len
+    #transform_r = (0.5*np.sqrt(5)/r_side)*origin_len
+
+    transform_l = (np.sqrt(1+b**2)/l_side)*origin_len
+    transform_r = (np.sqrt(1+b**2)/r_side)*origin_len
+    transform_wid = origin_wid*(b/(k+b))
+
+
+
+
+    #source coordinates
+
+    src_i = np.array([0, 0, 
+                    0, img.shape[0],
+                    img.shape[1], img.shape[0],
+                    img.shape[1], 0,]).reshape((4, 2))
+
+
+
+    #destination coordinates
+
+    dst_i = np.array([transform_center[0]-transform_wid*scale_factor, transform_center[1]-transform_l*scale_factor, 
+                    transform_center[0]-transform_wid*scale_factor, transform_center[1]+transform_l*scale_factor,
+                    transform_center[0]+transform_wid*scale_factor, transform_center[1]+transform_r*scale_factor,
+                    transform_center[0]+transform_wid*scale_factor, transform_center[1]-transform_r*scale_factor,]).reshape((4, 2))    
+
+
+
+    #using skimage’s transform module where ‘projective’ is our desired parameter
+    tform = transform.estimate_transform('projective', src_i, dst_i)
+    tf_img = transform.warp(img, tform.inverse)
+
+    #plotting the original image
+    plt.imshow(img)
+
+    #plotting the transformed image
+    fig, ax = plt.subplots()
+    ax.imshow(tf_img)
+    _ = ax.set_title('projective transformation')
+    plt.plot(transform_center[0],transform_center[1],'x')
+    plt.show()
+
+    # my own end
+    
+
     st.text("Press the button below to view Canny Edge Detection Technique")
     if st.button('Canny Edge Detector'):
         image = load_image("jerry.jpg")
@@ -99,7 +166,7 @@ def photo():
         
         st.image(thresh, use_column_width=True, clamp = True)
         st.image(img, use_column_width=True, clamp = True)
-    '''     
+        
 
 
   
